b33eaff5ff55ee6d8c63478f2338fd68bc55d984
sathishc <sat.hariharan@gmail.com>
2014-09-21 17:10:59 +0530

    fixing .gitignore

---
+++ platforms/android/ant-build/LocalReads-release-unsigned.apk.d
@@ -0,0 +1,4 @@
+/Users/SatSang/workspace/LocalReads/localreads-mobile/platforms/android/ant-build/LocalReads-release-unsigned.apk : \
+/Users/SatSang/workspace/LocalReads/localreads-mobile/platforms/android/ant-build/LocalReads.ap_ \
+/Users/SatSang/workspace/LocalReads/localreads-mobile/platforms/android/ant-build/classes.dex \
+/Users/SatSang/workspace/LocalReads/localreads-mobile/platforms/android/CordovaLib/ant-build/classes.jar \
---
+++ platforms/android/ant-gen/com/squareprism/localreads/ver1/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.squareprism.localreads.ver1;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = false;
+}
