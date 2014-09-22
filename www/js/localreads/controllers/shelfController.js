3ec6435f4b68ce9f1504f87b43eafc828f7482b7
sathishc <sat.hariharan@gmail.com>
2014-09-22 09:58:50 +0530

    Added ionic Popup based events

--- www/js/localreads/controllers/shelfController.js
+++ www/js/localreads/controllers/shelfController.js
@@ -1,12 +1,26 @@
 'use strict';
 
-localReadControllers.controller('ShelfCtrl', function($scope, $ionicPlatform,LocalReadsModelService,OwnershipsModel) {
+localReadControllers.controller('ShelfCtrl', function($scope, $ionicPlatform, $ionicPopup,
+                                                      LocalReadsModelService,OwnershipsModel) {
 
     $scope.shelfModel = OwnershipsModel;
 
-    $scope.removeBook = function($event){
-        var volumeId = $event.currentTarget.id;
-        LocalReadsModelService.removeBookFromShelf(volumeId);
+    $scope.showConfirm = function (data) {
+        var confirmPopup = $ionicPopup.confirm({
+            title: 'Delete Book',
+            template: 'Are you sure you want to remove this book from shelf?'
+        });
+        confirmPopup.then(function(res) {
+            if(res) {
+                $scope.removeBook(data);
+            } else {
+                console.log('You are not sure');
+            }
+        });
+    };
+
+    $scope.removeBook = function(ownership){
+        LocalReadsModelService.removeBookFromShelf(ownership.id);
     }
 
 });
